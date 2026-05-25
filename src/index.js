ab90d997a2a0ad3fcf1fae46cc629d7e6cd143e4
shane <devdummyacc101@gmail.com>
2026-05-25 18:56:58 -0400

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './containers/app'
 import reportWebVitals from './reportWebVitals';
+import 'tachyons'
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,3 +18,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
